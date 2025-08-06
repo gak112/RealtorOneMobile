8a32759b305bc2c9d1a96334a586b4667ed1297f
Gunda Ajay Kumar <ajay.a02@gmail.com>
2025-08-06 15:26:53 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RealtorOneMobile',
+  webDir: 'www'
+};
+
+export default config;
